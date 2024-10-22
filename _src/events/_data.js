74782158eb08d1838b3b319b174fee6774a4200e
tjaynl <info@tjay.nl>
2024-10-22 15:25:18 +0200

    Uploaded version 2

--- _src/events/_data.js
+++
@@ -1,9 +0,0 @@
-export const basename = "events"
-export const type = "event";
-export const layout = "layouts/event.vto";
-export const templateEngine = "vto,md";
-export const description = "Evenementen";
-
-export function url(page) {
-  return `/events/${page.data.basename}/`;
-}
--- _src/posts/_data.js
+++
@@ -1,9 +0,0 @@
-export const basename = "nieuws"
-export const type = "post";
-export const layout = "layouts/post.vto";
-export const templateEngine = "vto,md";
-export const description = "Nieuws artiekelen";
-
-export function url(page) {
-  return `/nieuws/${page.data.basename}/`;
-}
